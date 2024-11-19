56fbeac5a33f34aee1f9bc2873f7f6027b45c329
j_basterrechea@fpzornotza.com <1AW3-4@INF200886-1AW3.zornotzalh.eus>
2024-11-19 10:34:38 +0100

    otro git commit

--- pruebagit/src/pruebagit/prueba.java
+++ pruebagit/src/pruebagit/prueba.java
@@ -1,10 +1,12 @@
 package pruebagit;
 
+import java.util.Scanner;
+
 public class prueba {
 	/*
 	 * prueba 2.
 	 */
 	public static void main(String[] args) {
-		
+		Scanner scan=new Scanner(System.in);
 	}
 }
